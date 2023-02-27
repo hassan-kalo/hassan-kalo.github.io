aac1c2e550bcb488462df91cc16f68ef5a7b4a78
hassan-kalo <2430792@rts-365.dk>
2023-02-27 14:21:41 +0100

    demo ready. Messing search only

--- app.js
+++ app.js
@@ -1,54 +1,90 @@
-//Add tafsir and search
+//Add search
+let pageDiv;
+let pageInfoDiv;
 
-//const explainContainer = document.getElementById('explain-container')
-const pageDiv = document.createElement("div");
-const pageInfoDiv = document.createElement("div");
-pageInfoDiv.classList.add("pageInfoDiv");
-pageDiv.classList.add("page");
-document.body.insertAdjacentElement("afterbegin", pageDiv);
-
-const nextPageBtn = document.createElement("div");
-nextPageBtn.classList.add("next-page-btn");
-nextPageBtn.textContent = "❯";
-const prevPageBtn = document.createElement("div");
-prevPageBtn.classList.add("prev-page-btn");
-prevPageBtn.textContent = "❮";
-document.body.append(prevPageBtn, nextPageBtn);
-
-const tafsirsContainer = document.createElement("div");
-tafsirsContainer.classList.add("tafsirs-container");
-pageDiv.after(tafsirsContainer);
-//document.body.insertBefore(tafsirsContainer, pageDiv.nextSibling)
-
-let pageNumCounter = parseInt(localStorage.getItem("lastPage")) || 1;
 let suraName;
 let suraNum;
-if (pageNumCounter == 1) {
-  prevPageBtn.style.color = "gray";
-} else if (pageNumCounter == pages.length) {
-  nextPageBtn.style.color = "gray";
-} else {
-  nextPageBtn.style.color = "white";
-  prevPageBtn.style.color = "white";
+let pageNumCounter;
+
+let nextPageBtn;
+let prevPageBtn;
+
+let tafsirsContainer;
+
+initQuran();
+newContent();
+
+function initQuran(){
+  const wrapper = document.createElement("div");
+  wrapper.classList.add('wrapper')
+  document.body.append(wrapper)
+  pageDiv = document.createElement("div");
+  pageInfoDiv = document.createElement("div");
+  pageInfoDiv.classList.add("pageInfoDiv");
+  pageDiv.classList.add("page");
+  wrapper.insertAdjacentElement("afterbegin", pageDiv);
+
+  nextPageBtn = document.createElement("div");
+  nextPageBtn.classList.add("next-page-btn");
+  nextPageBtn.textContent = "❯";
+  prevPageBtn = document.createElement("div");
+  prevPageBtn.classList.add("prev-page-btn");
+  prevPageBtn.textContent = "❮";
+  wrapper.append(prevPageBtn, nextPageBtn);
+
+  tafsirsContainer = document.createElement("div");
+  tafsirsContainer.classList.add("tafsirs-container");
+  pageDiv.after(tafsirsContainer);
+  //wrapper.insertBefore(tafsirsContainer, pageDiv.nextSibling)
+
+  pageNumCounter = parseInt(localStorage.getItem("lastPage")) || 1;
+
+  if (pageNumCounter == 1) {
+    prevPageBtn.style.color = "gray";
+  } else if (pageNumCounter == pages.length) {
+    nextPageBtn.style.color = "gray";
+  } else {
+    nextPageBtn.style.color = "white";
+    prevPageBtn.style.color = "white";
+  }
+
+  const dictContainer = document.createElement("div");
+  dictContainer.classList.add("dict-container");
+  tafsirsContainer.prepend(dictContainer);
+  ////////////////////////////////////////////////////////
+  console.log(Object.keys(tafsirs));
+  const checkboxContainer = document.createElement("div");
+  checkboxContainer.classList.add("checkbox-container");
+  for (let i = 0; i < Object.keys(tafsirs).length; i++) {
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.dataset.tafsirName = Object.keys(tafsirs)[i];
+    checkbox.id = `checkbox${i}`;
+    checkbox.classList.add('checkbox')
+    checkbox.addEventListener("click", showTafsir);
+    const label = document.createElement("label");
+    label.setAttribute("for", `checkbox${i}`);
+    label.innerText = Object.keys(tafsirs)[i];
+    checkboxContainer.append(checkbox, label);
+  }
+  pageDiv.before(checkboxContainer, pageInfoDiv);
+  ///////////////////////////////////////
 }
 
-const dictContainer = document.createElement("div");
-dictContainer.classList.add("dict-container");
-dictContainer.classList.add("tafsir-container");
-tafsirsContainer.prepend(dictContainer);
 function explainWord(e) {
   const div = document.createElement("div");
   //div.classList.add('explained-word')
   div.innerHTML = `<span class="explained-word">${e.target.innerText}:</span> ${
     arQuranDictionary[e.target.innerText.replace(arDiacritics, "")]
   }`;
-  dictContainer.append(div);
+  document.querySelector('.dict-container').append(div);
 }
 
-// /[\u0660-\u0669]/ any ar digit
-
 function showTafsir() {
+  console.log(this)
   if (this.checked) {
+    console.log(this.id,this.checked)
+    //localStorage.setItem(this.id,this.checked)
     const div = document.createElement("div");
     div.classList.add("tafsir-container");
     div.id = "div" + this.dataset.tafsirName;
@@ -60,65 +96,83 @@ function showTafsir() {
 
     tafsirs[this.dataset.tafsirName][pageNumCounter - 1].forEach(
       (element, index) => {
-        const divAyaTafsir = document.createElement("div");
-        divAyaTafsir.classList.add("aya-tafsir");
-
-        const divAya = document.createElement("div");
-        divAya.classList.add("div-aya");
-        divAya.innerText =
-          pages[pageNumCounter - 1].split(/[\u0660-\u0669]/)[index];
-        //divAya.innerText = pages[pageNumCounter-1].match(/.+[\u0660-\u0669]+۝/)[index]
-        //divAya.innerText = pages[pageNumCounter-1].match(/^[^\u0660-\u0669۝]+\u0660-\u0669۝$/)[index]
-        //divAya.innerText = pages[pageNumCounter-1].match(/(?=\d\\۝)/)[index]
-        //divAya.innerText = pages[pageNumCounter-1].match(/[^۝]/)[index]
-
-        const divTafsir = document.createElement("div");
-        divTafsir.classList.add("div-tafsir");
-        divTafsir.innerText = element;
-
-        divAyaTafsir.append(divAya, divTafsir);
-        div.append(divAyaTafsir);
+        //console.log(element)
+        
+          const divAyaTafsir = document.createElement("div");
+          divAyaTafsir.classList.add("aya-tafsir");
+  
+          const divAya = document.createElement("div");
+          divAya.classList.add("div-aya");
+          //from page string get an array of ayas
+          divAya.innerText = pages[pageNumCounter - 1].split(/[\u0660-\u0669]/).filter(str => str !== '')[index];
+          //console.log(pages[pageNumCounter - 1].split(/[\u0660-\u0669]/).filter(str => str !== '')[index])
+          //divAya.innerText = pages[pageNumCounter-1].match(/.+[\u0660-\u0669]+۝/)[index]
+          //divAya.innerText = pages[pageNumCounter-1].match(/^[^\u0660-\u0669۝]+\u0660-\u0669۝$/)[index]
+          //divAya.innerText = pages[pageNumCounter-1].match(/(?=\d\\۝)/)[index]
+          //divAya.innerText = pages[pageNumCounter-1].match(/[^۝]/)[index]
+  
+          const divTafsir = document.createElement("div");
+          divTafsir.classList.add("div-tafsir");
+          divTafsir.innerText = element;
+  
+          divAyaTafsir.append(divAya, divTafsir);
+          div.append(divAyaTafsir);
+        
       }
     );
     tafsirsContainer.append(div);
-  } else {
-    console.log(this);
+  } 
+  else if(this.dataset){
+    //console.log('else if',this.dataset);
     document.getElementById("div" + this.dataset.tafsirName).remove();
     //document.getElementById
   }
-}
-console.log(Object.keys(tafsirs));
-const checkboxContainer = document.createElement("div");
-checkboxContainer.classList.add("checkbox-container");
-for (let i = 0; i < Object.keys(tafsirs).length; i++) {
-  const checkbox = document.createElement("input");
-  checkbox.setAttribute("type", "checkbox");
-  checkbox.dataset.tafsirName = Object.keys(tafsirs)[i];
-  checkbox.id = `checkbox${i}`;
-  checkbox.addEventListener("click", showTafsir);
-  const label = document.createElement("label");
-  label.setAttribute("for", `checkbox${i}`);
-  label.innerText = Object.keys(tafsirs)[i];
-  checkboxContainer.append(checkbox, label);
-}
-pageDiv.before(checkboxContainer, pageInfoDiv);
+  else{
+    tafsirsContainer.innerText = ''
+    const dictContainer = document.createElement("div");
+    dictContainer.classList.add("dict-container");
+    tafsirsContainer.prepend(dictContainer);
+    document.querySelectorAll('.checkbox').forEach((element)=>{
+      console.log(element.checked)
+      console.log(element)
+      if(element.checked){
 
-//document.body.append(checkboxContainer)
-//function ayaNumClickHandler(e){
-//    console.log('ayaNumClicked')
-/* checkboxContainer.style.display = 'grid'
-    checkboxContainer.style.top = e.target.offsetTop + 'px'
-    checkboxContainer.style.left = e.target.offsetLeft + 'px'
-    console.log(suraNum + e.target.innerText)
-    console.log(e.target.offsetTop) */
-//}
+        const div = document.createElement("div");
+        div.classList.add("tafsir-container");
+        div.id = "div" + element.dataset.tafsirName;
+
+        const h3 = document.createElement("h3");
+        h3.innerText = element.dataset.tafsirName;
+        div.append(h3);
+
+        tafsirs[element.dataset.tafsirName][pageNumCounter - 1].forEach(
+          (element, index) => {
+            const divAyaTafsir = document.createElement("div");
+            divAyaTafsir.classList.add("aya-tafsir");
+          
+            const divAya = document.createElement("div");
+            divAya.classList.add("div-aya");
+            divAya.innerText = pages[pageNumCounter - 1].split(/[\u0660-\u0669]/).filter(str => str !== '')[index];
+            const divTafsir = document.createElement("div");
+            divTafsir.classList.add("div-tafsir");
+            divTafsir.innerText = element;
+            divAyaTafsir.append(divAya, divTafsir);
+            div.append(divAyaTafsir);
+
+          });
+          tafsirsContainer.append(div);
+        } 
+      }  
+    )
+  }
+}
+
 function newContent() {
   if (pageNumCounter < 2) {
     suraName = "سورة الفاتحة";
     suraNum = 1;
   } else if (pageNumCounter < 50) {
-    //suraName = 'سورة البقرة'
-    suraName = "سورة الإخلاص    سورة الفلق    سورة الناس";
+    suraName = 'سورة البقرة'
     suraNum = 2;
   } else if (pageNumCounter < 77) {
     suraName = "سورة آل عمران";
@@ -133,6 +187,9 @@ function newContent() {
         suraName = 'سورة الإخلاص    سورة الفلق    سورة الناس'
     } */
   pageDiv.innerText = "";
+  /* document.querySelectorAll('checkbox').forEach((element)=>{
+    if(element)
+  }) */
   //let divAyasCreated = false
   //let containerAyas
   let firstWordInFirstAyaFound = false;
@@ -211,13 +268,14 @@ function newContent() {
     }) */
   pageDiv.append(pageNumDiv);
 }
-newContent();
 
 nextPageBtn.addEventListener("click", () => {
+  //dictContainer.innerText = '';
   if (pageNumCounter < pages.length) {
     pageNumCounter++;
     localStorage.setItem("lastPage", pageNumCounter);
     newContent();
+    showTafsir()
     prevPageBtn.style.color = "white";
     if (pageNumCounter == pages.length) {
       nextPageBtn.style.color = "gray";
@@ -228,10 +286,12 @@ nextPageBtn.addEventListener("click", () => {
   }
 });
 prevPageBtn.addEventListener("click", () => {
+  //dictContainer.innerText = '';
   if (pageNumCounter > 1) {
     pageNumCounter--;
     localStorage.setItem("lastPage", pageNumCounter);
     newContent();
+    showTafsir()
     nextPageBtn.style.color = "white";
     if (pageNumCounter == 1) {
       prevPageBtn.style.color = "gray";
@@ -242,6 +302,18 @@ prevPageBtn.addEventListener("click", () => {
   }
 });
 
+
+
+
+//wrapper.append(checkboxContainer)
+//function ayaNumClickHandler(e){
+//    console.log('ayaNumClicked')
+/* checkboxContainer.style.display = 'grid'
+    checkboxContainer.style.top = e.target.offsetTop + 'px'
+    checkboxContainer.style.left = e.target.offsetLeft + 'px'
+    console.log(suraNum + e.target.innerText)
+    console.log(e.target.offsetTop) */
+//}
 //////////////////////////////////////////////
 
 /* const string = "إِنَّ ٱلَّذِينَ كَفَرُواْ سَوَآءٌ عَلَيۡهِمۡ ءَأَنذَرۡتَهُمۡ أَمۡ لَمۡ تُنذِرۡهُمۡ لَا يُؤۡمِنُونَ";
@@ -258,3 +330,4 @@ if (collator.compare(normalizedString, normalizedWordToFind) === 0) {
 } else {
   console.log(`The word "${wordToFind}" was not found in the string.`);
 } */
+
